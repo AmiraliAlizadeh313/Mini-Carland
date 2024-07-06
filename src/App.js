56f13e24e1636bdde14ff8f4b6d746b00d323ae4
Amirali Alizadeh <amirali.alizadeh.dev.313@gmail.com>
2024-07-06 17:18:00 +0330

    Build some statics for your project but they are still not completely ready

--- src/App.js
+++ src/App.js
@@ -1,10 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-      <>
-      </>
+    <>
+      <CarsList></CarsList>
+    </>
+  );
+}
+
+function CarsList() {
+  return (
+    <>
+      <ul className="cars-list">
+        <li className="cars-item">
+
+          <div class="item-image"><img src={require("./images/Toyotaimage.jpg")} alt="toyota"></img></div>
+          <div className="item-content">
+            
+              <div className="item-title">
+                <h2 className="item-model">Landcruiser </h2>
+                <span className="item-company"> Toyota</span>
+              </div>
+
+            <div class="item-marks">
+              <span className="item-madein">Made in 2023</span>
+              <span className="item-color">Black</span>
+            </div>
+          </div>
+        </li>
+        <li className="cars-item">
+
+          <div class="item-image"><img src={require("./images/Toyotaimage.jpg")} alt="toyota"></img></div>
+          <div className="item-content">
+            
+              <div className="item-title">
+                <h2 className="item-model">Landcruiser </h2>
+                <span className="item-company"> Toyota</span>
+              </div>
+
+            <div class="item-marks">
+              <span className="item-madein">Made in 2023</span>
+              <span className="item-color">Black</span>
+            </div>
+          </div>
+        </li>
+        <li className="cars-item">
+
+          <div class="item-image"><img src={require("./images/Toyotaimage.jpg")} alt="toyota"></img></div>
+          <div className="item-content">
+            
+              <div className="item-title">
+                <h2 className="item-model">Landcruiser </h2>
+                <span className="item-company"> Toyota</span>
+              </div>
+
+            <div class="item-marks">
+              <span className="item-madein">Made in 2023</span>
+              <span className="item-color">Black</span>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </>
   );
 }
 
