dad3dd162f56e4eb423ce34ca8bb6c83a6f38a98
Amirali Alizadeh <amirali.alizadeh.dev.313@gmail.com>
2024-07-07 12:14:51 +0330

    The styling of cars list almost done but this project is an exercise project therefore we are focusing on react not css

--- src/App.js
+++ src/App.js
@@ -11,48 +11,102 @@ function CarsList() {
     <>
       <ul className="cars-list">
         <li className="cars-item">
-
-          <div class="item-image"><img src={require("./images/Toyotaimage.jpg")} alt="toyota"></img></div>
+          <div class="item-image">
+            <img src={require("./images/Toyotaimage.jpg")} alt="toyota"></img>
+          </div>
           <div className="item-content">
-            
-              <div className="item-title">
-                <h2 className="item-model">Landcruiser </h2>
-                <span className="item-company"> Toyota</span>
-              </div>
+            <div className="item-title">
+              <h2 className="item-model">Landcruiser </h2>
+              <span className="item-company"> Toyota</span>
+            </div>
 
             <div class="item-marks">
+              <span>Price $1000000</span>
               <span className="item-madein">Made in 2023</span>
               <span className="item-color">Black</span>
             </div>
           </div>
         </li>
         <li className="cars-item">
-
-          <div class="item-image"><img src={require("./images/Toyotaimage.jpg")} alt="toyota"></img></div>
+          <div class="item-image">
+            <img src={require("./images/Toyotaimage.jpg")} alt="toyota"></img>
+          </div>
           <div className="item-content">
-            
-              <div className="item-title">
-                <h2 className="item-model">Landcruiser </h2>
-                <span className="item-company"> Toyota</span>
-              </div>
+            <div className="item-title">
+              <h2 className="item-model">Landcruiser </h2>
+              <span className="item-company"> Toyota</span>
+            </div>
 
             <div class="item-marks">
+              <span>Price $1000000</span>
               <span className="item-madein">Made in 2023</span>
               <span className="item-color">Black</span>
             </div>
           </div>
         </li>
         <li className="cars-item">
-
-          <div class="item-image"><img src={require("./images/Toyotaimage.jpg")} alt="toyota"></img></div>
+          <div class="item-image">
+            <img src={require("./images/Toyotaimage.jpg")} alt="toyota"></img>
+          </div>
           <div className="item-content">
-            
-              <div className="item-title">
-                <h2 className="item-model">Landcruiser </h2>
-                <span className="item-company"> Toyota</span>
-              </div>
+            <div className="item-title">
+              <h2 className="item-model">Landcruiser </h2>
+              <span className="item-company"> Toyota</span>
+            </div>
 
             <div class="item-marks">
+              <span>Price $1000000</span>
+              <span className="item-madein">Made in 2023</span>
+              <span className="item-color">Black</span>
+            </div>
+          </div>
+        </li>
+        <li className="cars-item">
+          <div class="item-image">
+            <img src={require("./images/Toyotaimage.jpg")} alt="toyota"></img>
+          </div>
+          <div className="item-content">
+            <div className="item-title">
+              <h2 className="item-model">Landcruiser </h2>
+              <span className="item-company"> Toyota</span>
+            </div>
+
+            <div class="item-marks">
+              <span>Price $1000000</span>
+              <span className="item-madein">Made in 2023</span>
+              <span className="item-color">Black</span>
+            </div>
+          </div>
+        </li>
+        <li className="cars-item">
+          <div class="item-image">
+            <img src={require("./images/Toyotaimage.jpg")} alt="toyota"></img>
+          </div>
+          <div className="item-content">
+            <div className="item-title">
+              <h2 className="item-model">Landcruiser </h2>
+              <span className="item-company"> Toyota</span>
+            </div>
+
+            <div class="item-marks">
+              <span>Price $1000000</span>
+              <span className="item-madein">Made in 2023</span>
+              <span className="item-color">Black</span>
+            </div>
+          </div>
+        </li>
+        <li className="cars-item">
+          <div class="item-image">
+            <img src={require("./images/Toyotaimage.jpg")} alt="toyota"></img>
+          </div>
+          <div className="item-content">
+            <div className="item-title">
+              <h2 className="item-model">Landcruiser </h2>
+              <span className="item-company"> Toyota</span>
+            </div>
+
+            <div class="item-marks">
+              <span>Price $1000000</span>
               <span className="item-madein">Made in 2023</span>
               <span className="item-color">Black</span>
             </div>
